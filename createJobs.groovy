3aa1234bc97e04b6eb08a8903146931ff9aefef0
patilmaheshm <mmpatil_98@yahoo.com>
2021-05-05 14:55:15 -0400

    lastest error fixes

--- createJobs.groovy
+++ createJobs.groovy
@@ -12,9 +12,9 @@ pipelineJob('theme-park-job') {
             scm {
                 git {
                     remote {
-                        url 'https://github.com/tkgregory/spring-boot-api-example.git'
+                        url 'https://github.com/patilmaheshm/ThemeParks.git'
                     }
-                    branch 'master'
+                    branch 'main'
                 }
             }
         }
