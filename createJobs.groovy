cc3756e8bd844aaa6d6e1f70fe79fbdd1fa2c807
patilmaheshm <mmpatil_98@yahoo.com>
2021-05-05 18:14:17 -0400

    added docker imagge job

--- createJobs.groovy
+++ createJobs.groovy
@@ -20,3 +20,18 @@ pipelineJob('theme-park-job') {
         }
     }
 }
+pipelineJob('theme-park-job-docker') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url 'https://github.com/patilmaheshm/ThemeParks.git'
+                    }
+                    branch 'main'
+                    scriptPath('Jenkinsfile-docker')
+                }
+            }
+        }
+    }
+}
