01ec1f784f9e55df539e213016726acd01b89be0
patilmaheshm <mmpatil_98@yahoo.com>
2021-05-05 14:29:23 -0400

    chnaged to pieline

--- pipelineJob.groovy
+++ pipelineJob.groovy
@@ -14,17 +14,5 @@ pipeline {
         }
     }
 }
-pipelineJob('theme-park-job') {
-    definition {
-        cpsScm {
-            scm {
-                git {
-                    remote {
-                        url 'https://github.com/patilmaheshm/ThemeParks.git'
-                    }
-                    branch 'main'
-                }
-            }
-        }
-    }
-}
+
+
